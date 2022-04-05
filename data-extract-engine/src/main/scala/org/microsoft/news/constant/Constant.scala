c3684edf177d331ecbcb520f58c8e0f56658f4b2
adnanrahin <adnan.rahin13@gmail.com>
2022-04-05 02:13:37 -0400

    value exception handle

--- data-extract-engine/src/main/scala/org/microsoft/news/constant/Constant.scala
+++ data-extract-engine/src/main/scala/org/microsoft/news/constant/Constant.scala
@@ -6,7 +6,7 @@ object Constant {
   final val WIKIDATAID = "WikidataId"
   final val CONFIDENCE = "Confidence"
   final val TYPE = "Type"
-  final val OCCURRENCEOFFSETS = "OccurrenceOffsets*"
+  final val OCCURRENCEOFFSETS = "OccurrenceOffsets"
   final val SURFACEFORMS= "SurfaceForms"
 
 }
