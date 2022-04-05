b5f7678ffb2cfd06dbaff35d2d1444298051e382
adnanrahin <adnan.rahin13@gmail.com>
2022-04-04 21:15:41 -0400

    update

--- data-extract-engine/src/main/scala/org/microsoft/news/DataExtractEngineHelper.scala
+++ data-extract-engine/src/main/scala/org/microsoft/news/DataExtractEngineHelper.scala
@@ -4,6 +4,8 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
+import scala.util.parsing.json.JSON
+
 object DataExtractEngineHelper {
 
   def main(args: Array[String]): Unit = {
@@ -15,6 +17,26 @@ object DataExtractEngineHelper {
       .appName("DataExtractEngine")
       .master("local[*]")
       .getOrCreate()
+
+    val l: List[Any] = List(Map("A" -> "OMEGA-TEST", "B" -> List('A', 'B')), "String Test")
+
+    //l.foreach(list => println(list))
+
+    /* l.foreach {
+       value => {
+         val printVal = value match {
+           case map: Map[String, Any] => {
+             map.map {
+               case (key, mapVal) => {
+                 println(key + " : " + mapVal)
+               }
+             }
+           }
+           case _ => println(value)
+         }
+       }
+     }
+ */
 
     val sc = spark.sparkContext
     val path: String = args(0)
@@ -28,7 +50,25 @@ object DataExtractEngineHelper {
     testRdd
       .map(row => row.split("\t", -1))
       .foreach(
-        str => println(str(str.length - 1))
+        str => {
+          val test = JSON.parseFull(str(str.length - 1)).toList
+
+          test.map(
+            value => {
+              val printVal = value match {
+                case map: Map[String, Any] => {
+                  map.map {
+                    case (key, mapVal) => {
+                      println(key + " : " + mapVal)
+                    }
+                  }
+                }
+                case _ => println(value)
+              }
+            }
+          )
+
+        }
       )
 
   }
