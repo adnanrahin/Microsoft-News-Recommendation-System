9c56db656b19a59579cd58939f5352cde431cecf
adnanrahin <adnan.rahin13@gmail.com>
2022-04-04 21:39:18 -0400

    update

--- data-extract-engine/src/main/scala/org/microsoft/news/DataExtractEngineHelper.scala
+++ data-extract-engine/src/main/scala/org/microsoft/news/DataExtractEngineHelper.scala
@@ -51,22 +51,31 @@ object DataExtractEngineHelper {
       .map(row => row.split("\t", -1))
       .foreach(
         str => {
-          val test = JSON.parseFull(str(str.length - 1)).toList
+          val titlesEntities = JSON.parseFull(str(str.length - 1)).toList
 
-          test.map(
-            value => {
-              val printVal = value match {
-                case map: Map[String, Any] => {
-                  map.map {
-                    case (key, mapVal) => {
-                      println(key + " : " + mapVal)
-                    }
-                  }
-                }
-                case _ => println(value)
-              }
+          titlesEntities.map {
+            list => {
+              val label = list.asInstanceOf[List[Map[String, Any]]].map(map => map("Label").toString)
+              val wikiDataId = list.asInstanceOf[List[Map[String, Any]]].map(map => map("WikidataId").toString)
+              val confidence = list.asInstanceOf[List[Map[String, Any]]].map(map => map("Confidence").toString)
+              val titleType = list.asInstanceOf[List[Map[String, Any]]].map(map => map("Type").toString)
+              val occurrenceOffsets = list.asInstanceOf[List[Map[String, Any]]].map(map => map("OccurrenceOffsets").toString)
+              val surfaceForms = list.asInstanceOf[List[Map[String, Any]]].map(map => map("SurfaceForms").toString)
+
+              val wikiDataIdString = wikiDataId
+                .map(entity =>
+                  entity.toString.replace("List(", "").replace(")", ""))
+              val entitiesListString = "[" + wikiDataIdString.mkString(",") + "]"
+              val labelString = "[" + label.mkString(",") + "]"
+              val confidenceString = "[" + confidence.mkString(",") + "]"
+              val titleTypeString = "[" + titleType.mkString(",") + "]"
+              val occurrenceOffsetsString = "[" + occurrenceOffsets.mkString(",") + "]"
+              val surfaceFormsString = "[" + surfaceForms.mkString(",") + "]"
+
+              println(s"$labelString\t$entitiesListString\t$confidenceString\t$titleTypeString\t$occurrenceOffsetsString\t$surfaceFormsString")
+              println()
             }
-          )
+          }
 
         }
       )
