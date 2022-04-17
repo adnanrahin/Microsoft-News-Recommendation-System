28ecd192736e634157893cbb073c4b382343267c
adnanrahin <adnan.rahin13@gmail.com>
2022-04-16 23:32:38 -0400

    update

--- data-extract-engine/src/main/scala/org/microsoft/news/DataExtractEngine.scala
+++ data-extract-engine/src/main/scala/org/microsoft/news/DataExtractEngine.scala
@@ -3,9 +3,9 @@ package org.microsoft.news
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
-import org.microsoft.news.dataloader.NewsDataLoader
+import org.microsoft.news.dataloader.{BehaviorsDataLoader, NewsDataLoader}
 import org.microsoft.news.datawriter.DataFileWriterLocal
-import org.microsoft.news.entity.News
+import org.microsoft.news.entity.{Behaviors, News}
 
 object DataExtractEngine {
 
@@ -31,6 +31,14 @@ object DataExtractEngine {
     DataFileWriterLocal.dataWriter(dataFrame = newsDF,
       dataPath = transformDataPath,
       directoryName = "newsdata")
+
+    val behaviorsDataLoader = new BehaviorsDataLoader(s"$initialDataPath/behaviors.tsv", spark)
+    val behaviorsRDD: RDD[Behaviors] = behaviorsDataLoader.loadRDD()
+    val behaviorsDF = spark.createDataFrame(behaviorsRDD)
+
+    DataFileWriterLocal.dataWriter(dataFrame = behaviorsDF,
+      dataPath = transformDataPath,
+      directoryName = "behaviorsdatq")
 
   }
 
--- data-extract-engine/src/main/scala/org/microsoft/news/transformer/BehaviorsDataTransformer.scala
+++ data-extract-engine/src/main/scala/org/microsoft/news/transformer/BehaviorsDataTransformer.scala
@@ -10,10 +10,48 @@ object BehaviorsDataTransformer {
     val transformRDD: RDD[Behaviors] = newsRDD
       .map(row => row.split("\t", -1))
       .map(
-        str => {
+        col => {
 
+          val impressionId = col(0)
+          val userId = col(1)
+          val time: String = col(2)
+          val history: String = col(3)
+          val impression: String = col(4)
+
+          val clickCounter: Long = impression.split(" ")
+            .map {
+              str => {
+                if (str.endsWith("1")) {
+                  1
+                } else {
+                  0
+                }
+              }
+            }.foldLeft(0L)(_ + _)
+
+          val nonClickCounter: Long = impression.split(" ")
+            .map {
+              str => {
+                if (str.endsWith("0")) {
+                  1
+                } else {
+                  0
+                }
+              }
+            }.foldLeft(0L)(_ + _)
+
+          Behaviors(
+            impressionId,
+            userId,
+            time,
+            history,
+            impression,
+            clickCounter.toString,
+            nonClickCounter.toString
+          )
         }
       )
+    transformRDD
   }
 
 }
