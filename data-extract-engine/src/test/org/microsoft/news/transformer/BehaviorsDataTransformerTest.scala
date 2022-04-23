da0854d54ce5a2f91e31940bab5de1c6efaaf445
adnanrahin <adnan.rahin13@gmail.com>
2022-04-23 19:41:59 -0400

    data transformer unit test added

--- data-extract-engine/src/test/org/microsoft/news/transformer/BehaviorsDataTransformerTest.scala
+++ data-extract-engine/src/test/org/microsoft/news/transformer/BehaviorsDataTransformerTest.scala
@@ -42,4 +42,39 @@ class BehaviorsDataTransformerTest extends AnyFunSuite {
 
   }
 
+  test("testDataTransformer") {
+
+    val spark = SparkSession
+      .builder()
+      .appName("test")
+      .master(master = "local[*]")
+      .getOrCreate()
+
+    val sparkContext = spark.sparkContext
+
+    val actual = Seq("1\t" +
+      "U87243\t" +
+      "11/10/2019 11:30:54 AM\t" +
+      "N8668 N39081 N65259 N79529\t" +
+      "N94157-1 N39404-0 N108809-0 N78699-1 N71090-1 N40282-0 N31174-1"
+    )
+
+    val rdd: RDD[String] = sparkContext.parallelize(actual)
+
+    val expected = Behaviors(
+      "1",
+      "U87243",
+      "11/10/2019 11:30:54 AM",
+      "N8668 N39081 N65259 N79529",
+      "N78206-0 N26368-0 N7578-0 N94157-1 N39404-0 N108809-0 N78699-1 N71090-1 N40282-0 N31174-1",
+      4,
+      6
+    )
+
+    val actualBehaviors = BehaviorsDataTransformer.dataTransformer(rdd)
+
+    assert(actualBehaviors.collect().head != expected)
+
+  }
+
 }
