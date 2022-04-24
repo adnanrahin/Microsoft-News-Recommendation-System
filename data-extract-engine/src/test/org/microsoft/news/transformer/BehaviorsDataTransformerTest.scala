0c9111df1c8bf334365b8ec08aa7be438de2a73f
adnanrahin <adnan.rahin13@gmail.com>
2022-04-23 20:49:47 -0400

    unit test added behaviors

--- data-extract-engine/src/test/org/microsoft/news/transformer/BehaviorsDataTransformerTest.scala
+++ data-extract-engine/src/test/org/microsoft/news/transformer/BehaviorsDataTransformerTest.scala
@@ -7,7 +7,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class BehaviorsDataTransformerTest extends AnyFunSuite {
 
-  test("testDataTransformer") {
+  test("testDataTransformerMatch") {
 
     val spark = SparkSession
       .builder()
@@ -42,7 +42,7 @@ class BehaviorsDataTransformerTest extends AnyFunSuite {
 
   }
 
-  test("testDataTransformer") {
+  test("testDataTransformerMisMatch") {
 
     val spark = SparkSession
       .builder()
@@ -56,7 +56,7 @@ class BehaviorsDataTransformerTest extends AnyFunSuite {
       "U87243\t" +
       "11/10/2019 11:30:54 AM\t" +
       "N8668 N39081 N65259 N79529\t" +
-      "N94157-1 N39404-0 N108809-0 N78699-1 N71090-1 N40282-0 N31174-1"
+      "N78206-0 N26368-0 N7578-0 N94157-1 N39404-0 N108809-0 N78699-1 N71090-1 N40282-0 N31174-1"
     )
 
     val rdd: RDD[String] = sparkContext.parallelize(actual)
@@ -66,7 +66,7 @@ class BehaviorsDataTransformerTest extends AnyFunSuite {
       "U87243",
       "11/10/2019 11:30:54 AM",
       "N8668 N39081 N65259 N79529",
-      "N78206-0 N26368-0 N7578-0 N94157-1 N39404-0 N108809-0 N78699-1 N71090-1 N40282-0 N31174-1",
+      "N94157-1 N39404-0 N108809-0 N78699-1 N71090-1 N40282-0 N31174-1",
       4,
       6
     )
@@ -76,5 +76,4 @@ class BehaviorsDataTransformerTest extends AnyFunSuite {
     assert(actualBehaviors.collect().head != expected)
 
   }
-
 }
