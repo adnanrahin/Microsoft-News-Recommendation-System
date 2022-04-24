f0c68024018e887471b4d85016d7e22dbe7593cf
adnanrahin <adnan.rahin13@gmail.com>
2022-04-23 20:55:11 -0400

    update

---
+++ data-extract-engine/src/test/org/microsoft/news/transformer/NewsDataTransformerTest.scala
@@ -0,0 +1,13 @@
+package org.microsoft.news.transformer
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class NewsDataTransformerTest extends AnyFunSuite {
+
+  test("newsDataTransformer") {
+
+
+
+  }
+
+}
