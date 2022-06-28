2953cfa9f114dafeab6bfd3bb36a74789dab141c
adnanrahin <adnan.rahin13@gmail.com>
2022-06-27 23:43:32 -0400

    dependency added for data extract

--- data-aggregator-engine/src/main/scala/org/microsoft/news/data/aggeragator/DataAggregatorEngine.scala
+++ data-aggregator-engine/src/main/scala/org/microsoft/news/data/aggeragator/DataAggregatorEngine.scala
@@ -1,11 +1,16 @@
 package org.microsoft.news.data.aggeragator
 
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+import org.microsoft.news.data.extractor.data_reader.{BehaviorsDataReader, NewsDataReader}
+import org.microsoft.news.data.extractor.data_schemas.{Behaviors, News}
+import org.microsoft.news.data.extractor.data_writer.DataFileWriterLocal
 
 object DataAggregatorEngine {
- 
+
   def main(args: Array[String]): Unit = {
+
 
     Logger.getLogger("org").setLevel(Level.ERROR)
 
@@ -18,6 +23,23 @@ object DataAggregatorEngine {
       .appName("DataExtractEngine")
       .master(master = masterNode)
       .getOrCreate()
+
+
+    val newsDataLoader = new NewsDataReader(s"$initialDataPath/news.tsv", spark)
+    val newsRDD: RDD[News] = newsDataLoader.readDataToRDD()
+    val newsDF = spark.createDataFrame(newsRDD)
+
+    DataFileWriterLocal.dataWriter(dataFrame = newsDF,
+      dataPath = transformDataPath,
+      directoryName = "newsdata")
+
+    val behaviorsDataLoader = new BehaviorsDataReader(s"$initialDataPath/behaviors.tsv", spark)
+    val behaviorsRDD: RDD[Behaviors] = behaviorsDataLoader.readDataToRDD()
+    val behaviorsDF = spark.createDataFrame(behaviorsRDD)
+
+    DataFileWriterLocal.dataWriter(dataFrame = behaviorsDF,
+      dataPath = transformDataPath,
+      directoryName = "behaviorsdata")
 
   }
 
